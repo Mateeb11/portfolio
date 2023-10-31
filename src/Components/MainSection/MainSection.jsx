671899ea6cad6c11ea611f32da4a92f977c8bf61
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-10-31 17:26:57 +0300

    Adding my picture

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faReact,
+  faAngular,
+  faSquareJs,
+} from "@fortawesome/free-brands-svg-icons";
+
 import classes from "./MainSection.module.scss";
 
 import Skills from "./Skills/Skills";
@@ -5,16 +12,30 @@ import Skills from "./Skills/Skills";
 import UnderLine from "../../UI/Underline";
 import Button from "../../UI/Button";
 
-import reactLogo from "../../assets/react.svg";
+import picture from "../../assets/Picture.png";
 
 export default function MainSection() {
   return (
     <section className={classes.container}>
       <div className={classes.description}>
-        <img
-          src={reactLogo}
-          alt="React logo rotating 360 degree all the time"
-        />
+        <div className={classes.picture}>
+          <img src={picture} alt="A picture of me smiling" />
+          <FontAwesomeIcon
+            icon={faReact}
+            className={`${classes.icon} ${classes.reactIcon}`}
+            size="2x"
+          />
+          <FontAwesomeIcon
+            icon={faAngular}
+            className={`${classes.icon} ${classes.angularIcon}`}
+            size="2x"
+          />
+          <FontAwesomeIcon
+            icon={faSquareJs}
+            className={`${classes.icon} ${classes.jsIcon}`}
+            size="2x"
+          />
+        </div>
         <div className={classes.info}>
           <h1>
             Welcome to my page, I’m <UnderLine>Mateeb Alharbi</UnderLine>
