04fe8b0909be5fe14d22a67b0537671614d15aa5
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-09-10 09:58:04 +0300

    Added experience section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -5,7 +5,29 @@ import SectionTitle from "../../UI/SectionTitle";
 export default function Experience() {
   return (
     <section>
-      <SectionTitle title="EXPERIENCE" />
+      <SectionTitle title="EXPERIENCE" smallMargin={true} />
+      <div className={classes.container}>
+        <div className={classes.experience}>
+          <span>Elite Specialist | Tech GDP</span>
+          <span className={classes.small}>Mobily · Full Time</span>
+          <span className={classes.small}>06/2024 - Present</span>
+        </div>
+        <div className={classes.experience}>
+          <span>Junior Software Engineer</span>
+          <span className={classes.small}>WAKI · Full Time</span>
+          <span className={classes.small}>11/2023 - 06/2024</span>
+        </div>
+        <div className={classes.experience}>
+          <span>Junior Software Developer | Misk Traineeship Program</span>
+          <span className={classes.small}>WAKI · Internship</span>
+          <span className={classes.small}>08/2023 - 11/2023</span>
+        </div>
+        <div className={classes.experience}>
+          <span>IOT and Software Development</span>
+          <span className={classes.small}>Smart Methods · Internship</span>
+          <span className={classes.small}>06/2022 - 08/2022</span>
+        </div>
+      </div>
     </section>
   );
 }
--- src/UI/SectionTitle.jsx
+++ src/UI/SectionTitle.jsx
@@ -1,5 +1,9 @@
 import classes from "./SectionTitle.module.scss";
 
-export default function SectionTitle({ title }) {
-  return <h2 className={classes.title}>{title}</h2>;
+export default function SectionTitle({ title, smallMargin }) {
+  return (
+    <h2 className={`${classes.title} ${smallMargin && classes.smallMargin}`}>
+      {title}
+    </h2>
+  );
 }
