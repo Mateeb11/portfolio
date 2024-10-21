83fbf328e16a8740a98b7a1585de60148c14d35c
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-10-21 09:39:17 +0300

    Removed experience milestone Dot icon

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -8,34 +8,26 @@ export default function Experience() {
   return (
     <section>
       <SectionTitle title="EXPERIENCE" smallMargin={true} />
-      <div className={classes.experienceContainer}>
-        <div className={classes.milestoneContainer}>
-          <img src={whiteCircle} alt="" />
-          <img src={whiteCircle} alt="" />
-          <img src={whiteCircle} alt="" />
-          <img src={whiteCircle} alt="" />
+      <div className={classes.container}>
+        <div className={classes.experience}>
+          <span>Elite Specialist | Tech GDP</span>
+          <span className={classes.small}>Mobily · Full Time</span>
+          <span className={classes.small}>06/2024 - Present</span>
         </div>
-        <div className={classes.container}>
-          <div className={classes.experience}>
-            <span>Elite Specialist | Tech GDP</span>
-            <span className={classes.small}>Mobily · Full Time</span>
-            <span className={classes.small}>06/2024 - Present</span>
-          </div>
-          <div className={classes.experience}>
-            <span>Junior Software Engineer</span>
-            <span className={classes.small}>WAKI · Full Time</span>
-            <span className={classes.small}>11/2023 - 06/2024</span>
-          </div>
-          <div className={classes.experience}>
-            <span>Junior Software Developer | Misk Traineeship Program</span>
-            <span className={classes.small}>WAKI · Internship</span>
-            <span className={classes.small}>08/2023 - 11/2023</span>
-          </div>
-          <div className={classes.experience}>
-            <span>IOT and Software Development</span>
-            <span className={classes.small}>Smart Methods · Internship</span>
-            <span className={classes.small}>06/2022 - 08/2022</span>
-          </div>
+        <div className={classes.experience}>
+          <span>Junior Software Engineer</span>
+          <span className={classes.small}>WAKI · Full Time</span>
+          <span className={classes.small}>11/2023 - 06/2024</span>
+        </div>
+        <div className={classes.experience}>
+          <span>Junior Software Developer | Misk Traineeship Program</span>
+          <span className={classes.small}>WAKI · Internship</span>
+          <span className={classes.small}>08/2023 - 11/2023</span>
+        </div>
+        <div className={classes.experience}>
+          <span>IOT and Software Development</span>
+          <span className={classes.small}>Smart Methods · Internship</span>
+          <span className={classes.small}>06/2022 - 08/2022</span>
         </div>
       </div>
     </section>
