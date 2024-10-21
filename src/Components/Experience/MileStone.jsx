5ed353935929a2d7130d56e97e7be7a6d9804370
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-10-21 10:55:33 +0300

    Added pulse effect to the current milestone

--- src/Components/Experience/MileStone.jsx
+++ src/Components/Experience/MileStone.jsx
@@ -4,6 +4,7 @@ export default function MileStone({ title, company, time, current }) {
   return (
     <li className={`${classes.milestone} ${current && classes.current}`}>
       <div className={classes.content}>
+        {current && <span className={classes.pulse}></span>}
         <span>{title}</span>
         <span className={classes.small}>{company}</span>
         <span className={classes.small}>{time}</span>
