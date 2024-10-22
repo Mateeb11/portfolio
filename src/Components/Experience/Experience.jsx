ee7c18f24316e26c0d135235729c080f966c9b12
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-10-22 13:56:06 +0300

    Added milestone description

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -9,15 +9,31 @@ export default function Experience() {
       <SectionTitle title="EXPERIENCE" smallMargin={true} />
       <ul className={classes.container}>
         <MileStone
+          current={true}
           title="Elite Specialist | Tech GDP"
           company="Mobily · Full Time"
           time="06/2024 - Present"
-          current={true}
+          description={[
+            "Rotation over Mobily tech departments",
+            "Learn how each department operates",
+            "Learn tech departments environment in telecommunication sector",
+            "Contribute in day-to-day tasks",
+          ]}
         ></MileStone>
         <MileStone
           title="Junior Software Engineer"
           company="WAKI · Full Time"
           time="11/2023 - 06/2024"
+          description={[
+            "Analyze requirements and provide clean and reliable technical solutions",
+            "Work with UX/UI designers to transform designs into frontend code with Angular JS",
+            "Work with back-end developers to integrate UI components with APIs and databases",
+            "Write high-quality, scalable, and reusable code",
+            "Write functional requirement documents and guides",
+            "Debugging & Troubleshooting",
+            "Continuously improve the user interface based on user feedback and testing",
+            "Code refactoring",
+          ]}
         ></MileStone>
         <MileStone
           title="Junior Software Developer | Misk Traineeship Program"
@@ -26,15 +42,14 @@ export default function Experience() {
           description={[
             "Fix responsiveness issues",
             "Debugging",
-            "Add new features",
-            "Refactor the code for old pages",
-            "Suggest new ideas and how to do it",
+            "Troubleshooting and fixing frontend issues",
           ]}
         ></MileStone>
         <MileStone
           title="IOT and Software Development"
           company="Smart Methods · Internship"
           time="06/2022 - 08/2022"
+          description={["Used Arduino", "Implemented web Text-to-Speech"]}
         ></MileStone>
       </ul>
     </section>
