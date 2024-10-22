1a78ed562ab7a72f3d419f61cbab88b687c9eaf4
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-10-22 11:06:13 +0300

    Added expandable section under milestone

--- src/Components/Experience/MileStone.jsx
+++ src/Components/Experience/MileStone.jsx
@@ -1,5 +1,9 @@
 import classes from "../Experience/Experience.module.scss";
 
+import { useCollapse } from "react-collapsed";
+
+import arrow from "../../assets/arrow-down.svg";
+
 export default function MileStone({
   title,
   company,
@@ -7,22 +11,36 @@ export default function MileStone({
   current,
   description = [],
 }) {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+
   return (
     <li className={`${classes.milestone} ${current && classes.current}`}>
-      <div className={classes.content}>
+      <div className={classes.content} {...getToggleProps()} tabIndex="-1">
         {current && <span className={classes.pulse}></span>}
         <span>{title}</span>
         <span className={classes.small}>{company}</span>
-        <span className={classes.small}>{time}</span>
-      </div>
-      {description && (
-        <div className={classes.description}>
-          {description.map((line, i) => (
-            <span className={classes.small} key={i}>
-              - {line}
-            </span>
-          ))}
+        <div className={classes.timeMore}>
+          <span className={classes.small}>{time}</span>
+          <button className={`${classes.collapseBtn} ${classes.small}`}>
+            {isExpanded ? "Less" : "More"}
+            <img
+              className={`${classes.arrow} ${isExpanded && classes.rotate}`}
+              src={arrow}
+            ></img>
+          </button>
         </div>
+      </div>
+      {description.length > 0 && (
+        <>
+          <p className={classes.description} {...getCollapseProps()}>
+            {description.map((line, i) => (
+              <span className={classes.small} key={i}>
+                - {line}
+                <br />
+              </span>
+            ))}
+          </p>
+        </>
       )}
     </li>
   );
